8572e3f88ded0028f5a4dd7567d0f17c72b38bf4
boggsjab@gmail.com <boggsjab@gmail.com>
2018-07-23 18:38:35 -0400

    bubble iterative

--- Java/BubbleSort/BubbleSort.java
+++ Java/BubbleSort/BubbleSort.java
@@ -1,5 +1,40 @@
 public class BubbleSort{
-	public static void main(String args[]){
 
+	public static void BubbleSort(int[] a){
+		boolean sorted=false;
+		int last=a.length-1;
+
+		while(!sorted){
+			sorted=true;
+			for(int i=0; i<last;i++){
+				if(a[i]>a[i+1]){
+					swap(a,i,i+1);
+					sorted=false;
+				}
+			}
+			last--;
+		}
+	}
+
+	public static void swap(int[] a, int i, int j){
+		int temp = a[i];
+		a[i]=a[j];
+		a[j]=temp;
+	}
+
+	public static void main(String args[]){
+		int[] a={2,5,34,6,46,53};
+		int[] b=new int[a.length];
+
+		for(int work:a){
+			System.out.print(work + " ");
+		}
+		System.out.println();
+
+		BubbleSort(a);
+		
+		for(int work:a){
+			System.out.print(work + " ");
+		}
 	}
 }
