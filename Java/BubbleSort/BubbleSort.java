37ac7921ebc67d1bdc25179644aca240b3c7a78b
boggsjab@gmail.com <boggsjab@gmail.com>
2018-07-23 19:35:55 -0400

    recurs bubble sort

--- Java/BubbleSort/BubbleSort.java
+++ Java/BubbleSort/BubbleSort.java
@@ -1,5 +1,14 @@
 public class BubbleSort{
 
+	// BubbleSort
+	//	compare consecutive values in an array (i, i+1)and swap them if
+	//	i<i+1, pushing the largest value to the end of the array.  repeat
+	//	until the entirety of the array is sorted
+	
+	//	-average/worst case complexity O(n^2)
+	//	-best case complexity O(n)
+
+	// iterative Bubble sort
 	public static void BubbleSort(int[] a){
 		boolean sorted=false;
 		int last=a.length-1;
@@ -16,6 +25,18 @@ public class BubbleSort{
 		}
 	}
 
+	// recursive Bubble sort
+	public static void BubbleSortRecurs(int[] a, int len){
+		if(len<=1){return;}
+		
+		for(int i=0; i<len-1;i++){
+			if(a[i]>a[i+1]){swap(a,i,i+1);}
+		}
+		BubbleSortRecurs(a, --len);
+	}
+	
+	// swap function exchanges the values at indeces
+	// i and j, in array a
 	public static void swap(int[] a, int i, int j){
 		int temp = a[i];
 		a[i]=a[j];
@@ -24,8 +45,9 @@ public class BubbleSort{
 
 	public static void main(String args[]){
 		int[] a={2,5,34,6,46,53};
-		int[] b=new int[a.length];
+		int[] b={3,6,2,25,32,31,26,4};
 
+		// iterative
 		for(int work:a){
 			System.out.print(work + " ");
 		}
@@ -36,5 +58,19 @@ public class BubbleSort{
 		for(int work:a){
 			System.out.print(work + " ");
 		}
+		System.out.println();
+		
+		// recursive
+		for(int work:b){
+			System.out.print(work + " ");
+		}
+		System.out.println();
+
+		BubbleSortRecurs(b, b.length);
+		
+		for(int work:b){
+			System.out.print(work + " ");
+		}
+		System.out.println();
 	}
 }
